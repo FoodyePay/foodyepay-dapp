a14c698238525fc5d544e626809cf87a822d4e54
Foody Coin <info@foodeycoin.io>
2025-04-28 07:12:16 -0400

    fix: update export/import for Providers component and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { ReactNode } from 'react'
-import { Providers } from './providers'
+import Providers from './providers';
 
 export const metadata: Metadata = {
   title: 'FoodyePay DApp',
